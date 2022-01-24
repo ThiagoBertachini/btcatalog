5069985ee855e62f189d72265b767c3e8e10dd59
ThiagoBertachini <thiago.bertachini@hotmail.com>
2022-01-24 18:45:12 -0300

    Backend project created

---
+++ backend/src/main/java/com/bertachini/btCatalog/BtCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.bertachini.btCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtCatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bertachini/btCatalog/BtCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bertachini.btCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
