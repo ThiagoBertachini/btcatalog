75d0d3dd9fc8a040a8ed26c408c9d4e3019ee441
ThiagoBertachini <thiago.bertachini@hotmail.com>
2022-01-31 20:10:50 -0300

    pagination

--- backend/src/main/java/com/bertachini/btCatalog/services/CategoryService.java
+++ backend/src/main/java/com/bertachini/btCatalog/services/CategoryService.java
@@ -8,6 +8,8 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -26,9 +28,9 @@ public class CategoryService {
 	
 	/*Busca lista de categorias dos produtos(todas) do catalago de banco de dados*/
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll(){				
-		return repository.findAll().stream()
-				.map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest){				
+		return repository.findAll(pageRequest)
+				.map(x -> new CategoryDTO(x));
 		
 		//List<CategoryDTO> list = repository.findaAll();
 		//return list.stram()...
