394f3e25f227dfaebee38c2a0b1e878242282cb5
ThiagoBertachini <thiago.bertachini@hotmail.com>
2022-03-06 21:50:56 -0300

    @Repository

--- backend/src/main/java/com/bertachini/btCatalog/repositories/RoleRepository.java
+++ backend/src/main/java/com/bertachini/btCatalog/repositories/RoleRepository.java
@@ -1,9 +1,11 @@
 package com.bertachini.btCatalog.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.bertachini.btCatalog.entities.Role;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 	
 	
